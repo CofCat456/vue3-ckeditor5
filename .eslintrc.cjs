61c00492028114aa3a79d3a843eebc5a83a3e433
CofCat456 <a25690756@gmail.com>
2023-07-08 03:37:44 +0800

    chore: 🐙 install eslint and prettier

---
+++ .eslintrc.cjs
@@ -0,0 +1,32 @@
+module.exports = {
+  root: true,
+  env: {
+    browser: true,
+    es2021: true
+  },
+  extends: [
+    'plugin:vue/vue3-essential',
+    'eslint:recommended',
+    'airbnb-base',
+    '@vue/eslint-config-prettier/skip-formatting'
+  ],
+  overrides: [],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module'
+  },
+  settings: {
+    'import/resolver': {
+      alias: {
+        map: [['@', './src']],
+        extensions: ['.js', '.vue']
+      }
+    },
+    'import/core-modules': ['vite', '@vitejs/plugin-vue']
+  },
+  plugins: ['vue'],
+  rules: {
+    semi: ['error', 'always'],
+    camelcase: ['off'],
+  }
+};
